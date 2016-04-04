3cbe90c638fe7802ee87c6c0a33d9db6579f9f69
Lingo.Blyde <weiliu32992016@qq.com>
2016-04-04 18:47:28 +0800

    Create Float2Int_Union.h

---
+++ Float2Int_Union.h
@@ -0,0 +1,16 @@
+#ifndef __FLOAT2INT_HEADER__
+#define __FLOAT2INT_HEADER__
+
+/**
+* Float2Int UNION
+* 
+* @解释：参考开发手册P33，使用联合体转换参数
+* @author: blyde 
+* @update:2015.7.11
+**/
+union Float2Int
+{
+	float 	fBuff;
+	int		iBuff;
+};
+#endif // __FLOAT2INT_HEADER__
